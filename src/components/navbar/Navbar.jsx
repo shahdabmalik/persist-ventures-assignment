18afb9feacdad44c844160545d62ba05246a3b53
shahdabmalik <shahdab888@gmail.com>
2023-10-14 23:30:52 +0530

    animation color update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,9 +13,9 @@ const Navbar = () => {
                 <img className="w-full" src="https://uploads-ssl.webflow.com/5ef95ae22a251e0c9030800e/5ef987561883fad47e90211c_Ideaflow%20white%201%403x.png" alt="logo" />
             </div>
             <div className="flex gap-5 text-sm font-inter" >
-                <Link to="#" className=" hidden sm:block" >Home</Link>
+                <Link to="#" className=" hidden sm:block hover:text-cyan-300" >Home</Link>
                 <Link to="#" className="text-cyan-400 hover:text-cyan-300 hover:scale-[1.03] transition-all hidden sm:block" >We&#39;re Hiring! JS, iOS +</Link>
-                <Link to="#" className=" hidden sm:block">Contact</Link>
+                <Link to="#" className=" hidden sm:block hover:text-cyan-300">Contact</Link>
                 <div className="sm:hidden text-white" onClick={() => setShow(!show)} ><AiOutlineMenu size={24} /></div>
             </div>
             <div className={"w-full h-screen z-50 sm:hidden fixed top-0  px-5 flex flex-col backdrop-blur-sm bg-slate-900 bg-opacity-90 transition-all duration-200" +(show ? " left-0 " : " left-[120%] ")} >
